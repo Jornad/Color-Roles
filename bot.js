a62852a9a7fecca14b0d23bfc1d1509ccb691091
Jornad <jordan.watkins0922@gmail.com>
2020-10-24 12:30:47 -0400

    started custom logger

--- bot.js
+++ bot.js
@@ -4,18 +4,49 @@ const colors = require("colors"); //Allows use of colors in console
 const clear = require("clear"); //Lets us clear the console
 const client = new Discord.Client(); //Create Discord client instance
 
+//Logger
+function mlog(status, type, tcolor, message, mcolor) {
+  let cmsg = [];  //Array for console message
+  let lmsg = [];  //Array for log message
+
+  switch (status) { //Add status to arrays
+    case "OK":
+      cmsg.push("[OK].green ");
+      lmsg.push("[OK] ");
+      break;
+    case "WARN":
+      cmsg.push("[WARN].yellow ");
+      lmsg.push("[WARN] ");
+      break;
+    case "ERROR":
+      cmsg.push("[ERROR].red ");
+      lmsg.push("[ERROR] ");
+      break;
+  }
+
+  cmsg.push("["+type+"]."+color); //Add type tag and color it to console message
+  lmsg.push("["+type+"]");  //Add type tag to log message
+
+  console.log(cmsg.prototype.toString());
+}
+
 //Does initial setup when client is ready
 client.on("ready", () => {
   clear();
   client.user.setUsername(config.username.toString());
+  mlog("OK", "LOGIN". "blue", "Cleared Console")
+  /*
   console.log("[OK] ".green + "[LOGIN] ".blue + "Cleared console".cyan);
   console.log("[OK] ".green + "[LOGIN] ".blue + "Connected".cyan);
   console.log("[OK] ".green + "[LOGIN] ".blue + "Logged in as: ".cyan + client.user.username + " - " + client.user.id);
+*/
 
   client.user.setActivity(config.prefix + "help"); //Set bot status to help message
+  /*
   console.log("[OK] ".green + "[LOGIN] ".blue + "Set game".cyan);
   console.log("[OK] ".green + "[LOGIN] ".blue + "I am ready!".cyan);
   console.log("\n\n\n");
+  */
 });
 
 //Main stuff
