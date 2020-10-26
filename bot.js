d1d1e1224d8f6a425c5354b467a26202fbdb1ec8
Jornad <jordan.watkins0922@gmail.com>
2020-10-26 00:01:09 -0400

    almost addeed nodemon but dont feel like setting up rn

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const config = require('./config.json'); //Load the config file
-//const logfile = require('./log.txt');
-const Discord = require('discord.js'); //Import discord.js
-const colors = require('colors'); //Allows use of colors in console
-const clear = require('clear'); //Lets us clear the console
+const config = require("./config.json"); //Load the config file
+//const logfile = require("./log.txt");
+const Discord = require("discord.js"); //Import discord.js
+const colors = require("colors"); //Allows use of colors in console
+const clear = require("clear"); //Lets us clear the console
 const client = new Discord.Client(); //Create Discord client instance
 
 //Logger
@@ -13,33 +13,33 @@ function mlog(status, type, message) {
   let ctime = [d.getHours(), d.getMinutes(), d.getSeconds()]; //makes an array of hh, mm, ss
 
   switch (status) {
-    case 'INFO': //Logs info
-      console.log('['.white + ctime.join(':').cyan + ' INFO'.green + ']: '.white + type.white + ' - '.white + message.white);
+    case "INFO": //Logs info
+      console.log("[".white + ctime.join(":").cyan + " INFO".green + "]: ".white + type.white + " - ".white + message.white);
       break;
-    case 'WARN':  //Logs warnings
-      console.log('['.white + ctime.join(':').cyan + ' WARN'.yellow + ']: ' + type.white + ' - '.white + message.white);
+    case "WARN":  //Logs warnings
+      console.log("[".white + ctime.join(":").cyan + " WARN".yellow + "]: " + type.white + " - ".white + message.white);
       break;
-    case 'ERROR': //logs errors
-      console.log('['.white + ctime.join(':').cyan + ' ERROR'.red + ']: ' + type.white + ' - '.white + message.white);
+    case "ERROR": //logs errors
+      console.log("[".white + ctime.join(":").cyan + " ERROR".red + "]: " + type.white + " - ".white + message.white);
       break;
   }
-}
+} 
 
 //Does initial setup when client is ready
-client.on('ready', () => {
+client.on("ready", () => {
   clear();
   client.user.setUsername(config.username.toString());
-  mlog('INFO', 'LOGIN', 'Cleared Console');
-  mlog('INFO', 'LOGIN', 'Connected');
-  mlog('INFO', 'LOGIN', 'Logged in as ' + client.user.username + ' ' + client.user.id);
+  mlog("INFO", "LOGIN", "Cleared Console");
+  mlog("INFO", "LOGIN", "Connected");
+  mlog("INFO", "LOGIN", "Logged in as " + client.user.username + " " + client.user.id);
 
-  client.user.setActivity(config.prefix + 'help'); //Set bot status to help message
-  mlog('INFO', 'SETUP', 'Set game to ' + config.prefix + 'help');
-  mlog('INFO', 'SETUP', 'I am ready!');
+  client.user.setActivity(config.prefix + "help"); //Set bot status to help message
+  mlog("INFO", "SETUP", "Set game to " + config.prefix + "help");
+  mlog("INFO", "SETUP", "I am ready!");
 });
 
 //Main stuff
-client.on('message', (message) => {
+client.on("message", (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return; //Ignore messages missing prefix or sent by another bot
 
 });
